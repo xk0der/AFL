f456b030483624a9a368b8f2a09f564cecb94584
xk0der <amit@xkoder.com>
2009-11-05 19:20:45 +0530

    Modified snippets and added tooltips

--- html_body.php
+++ html_body.php
@@ -2,13 +2,19 @@
 <div id="mainContainer">
     <form name="code" action="?e=1" method="POST">
         <div>
-            <div id="code_head">Code
-            
+            <div id="code_head">
+                <div style="float:left;">Code
+                    <img src="image/question.png" align="top" title="Write your code in the text box below" >
+                </div>
+            <div style="width:100%; text-align:right;">
+            Load example:
             <select id="code_snippet" onchange="var v = document.getElementById('code_snippet'); 
                                                 var p = document.getElementById('program');
                                                 p.value = v.value;"
                                                 >
-
+                    <option value="; Write your code below
+                    " 
+                    >-Select snippet-</option>
             <?php 
                 foreach($snippet as $key => $value) {
                     echo "<option value='".$snippet[$key]["code"]."'>".$snippet[$key]["name"]."</option>\n";
@@ -16,21 +22,26 @@
             ?>
 
             </select>
+            </div>
             
             </div>
             <textarea name="program" id="program" rows="10" cols="80"><?= AFL::$program ?></textarea>
         </div>
         <div>
-            <input type="submit" value="Execute">
+            <input type="submit" value="Execute" title="Passes the AFL code above to the server for execution.">
         </div>
     </form>
     <div id="outputContainer">
-        <div id="output_head">Output</div>
+        <div id="output_head">Output
+            <img src="image/question.png" align="top" title="Output of AFL code execution gets displayed below" >
+        </div>
         <pre><?= AFL::$output ?></pre>
     </div>
 </div>
 <div id="debug">
-<div id="debug_head">Internal Trace Dump</div>
+<div id="debug_head">Internal Trace Dump
+    <img src="image/question.png" align="top" title="Internal PHP state dumps for AFL." >
+</div>
 <pre>
 <?= AFL::$debugLog ?>
 </pre>
--- include/snippets.php
+++ include/snippets.php
@@ -7,6 +7,10 @@ $snippet['fibonacci'] = array (
 f 0 = 1
 f 1 = 1
 f n = + (f (- n 1)) (f (- n 2))
+; Find the 8th term of the series
+f 8
+
+; Press the [Execute] button below to run this code.
 ",
 );
 
@@ -16,6 +20,10 @@ $snippet['factorial'] = array (
 "; Factorial
 f 0 = 1
 f x = * x (f (- x 1))
+; Factorial 10
+f 10
+
+; Press the [Execute] button below to run this code.
 ",
 );
 
@@ -25,6 +33,10 @@ $snippet['power']= array (
 "; Power of a number
 f x 0 = 1
 f x y = * x (f x (- y 1))
+; 2 raised to the power 10
+f 2 10
+
+; Press the [Execute] button below to run this code.
 ",
 );
 
@@ -34,5 +46,9 @@ $snippet['series_summation']= array (
 "; Summation of series 
 series n = * 2 n
 sum n = + (series n) (series (- n 1))
+; Sum 5 terms of the series
+sum 5
+
+; Press the [Execute] button below to run this code.
 ",
 );
